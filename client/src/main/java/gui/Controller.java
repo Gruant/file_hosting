0fc1cafe240601f81df8a84bad1eec1e3d40a7a9
antongrutsin <grucinav@gmail.com>
2021-03-11 13:17:36 +0300

    Add fileType column

--- client/src/main/java/gui/Controller.java
+++ client/src/main/java/gui/Controller.java
@@ -1,9 +1,16 @@
 package gui;
 
+import core.FileInfo;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +19,13 @@ public class Controller implements Initializable {
     public TableView filesTable;
 
     public void initialize(URL location, ResourceBundle resources) {
+
+        TableColumn<FileInfo, String> typeColumn = new TableColumn<FileInfo, String>("Type");
+        typeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getType().getName()));
+        typeColumn.setPrefWidth(50);
+        typeColumn.setSortType(TableColumn.SortType.ASCENDING);
+
+        filesTable.getColumns().addAll(typeColumn);
 
     }
 
---
+++ core/src/main/java/core/FileInfo.java
@@ -0,0 +1,62 @@
+package core;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileInfo {
+    public enum FileType {
+        FILE("File"), DIRECTORY("Directory");
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        FileType(String name) {
+            this.name = name;
+        }
+    }
+
+    private String filename;
+    private long size;
+    private FileType type;
+
+    public FileInfo(Path path){
+        try {
+            this.filename = path.getFileName().toString();
+            this.size = Files.size(path);
+            this.type = Files.isDirectory(path) ? FileType.DIRECTORY : FileType.FILE;
+            if (this.type == FileType.DIRECTORY) {
+                this.size = -1L;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't get a info from file");
+        }
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public FileType getType() {
+        return type;
+    }
+
+    public void setType(FileType type) {
+        this.type = type;
+    }
+}
